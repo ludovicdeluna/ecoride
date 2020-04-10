ba10fa36020a1fcfd3bae3e46ceb755fb298aa54
Thomas P <thomas.petrachi@ecov.fr>
2020-04-10 17:00:48 +0200

    Add a test

---
+++ spec/requests/passenger_and_driver_share_a_ride_spec.rb
@@ -0,0 +1,67 @@
+require "rails_helper"
+
+RSpec.describe "Ride sharing between a driver and a passenger", :type => :request do
+
+  let(:driver){ User.create(email: "david@email.com") }
+  let(:passenger){User.create(email: "peter@email.com")}
+  let(:ride){Ride.create(departure: "ici", arrival: "la") }
+
+  it "creates a Widget and redirects to the Widget's page" do
+    post "/graphql", params: {
+      query: "mutation{
+        createDriverRide(
+          input: {
+          	userId: #{driver.id},
+          	rideId: #{ride.id}
+        	}
+        ){
+          driverRide{
+            id
+          }
+          errors
+        }
+      }"
+    }
+
+    expect(DriverRide.last.user).to eq(driver)
+    expect(DriverRide.last.ride).to eq(ride)
+
+    post "/graphql", params: {
+      query: "mutation{
+        createPassengerRide(
+          input: {
+          	userId: #{passenger.id},
+          	rideId: #{ride.id}
+        	}
+        ){
+          passengerRide{
+            id
+          }
+          errors
+        }
+      }"
+    }
+
+    expect(PassengerRide.last.user).to eq(passenger)
+    expect(PassengerRide.last.ride).to eq(ride)
+
+    post "/graphql", params: {
+      query: "mutation{
+        shareRide(
+          input: {
+          	driverRideId: #{DriverRide.last.id},
+          	passengerRideId: #{PassengerRide.last.id}
+        	}
+        ){
+          passengerRide{
+            id
+          }
+          errors
+        }
+      }"
+    }
+
+    p response.body
+    expect(PassengerRide.last.driver_ride).to eq(DriverRide.last)
+  end
+end
