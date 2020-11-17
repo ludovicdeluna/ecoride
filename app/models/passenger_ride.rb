ddd0fa4c5a2292d4b327222e6287014bc2ada598
Ludovic de Luna <ludovic.deluna@gmail.com>
2020-11-17 10:19:08 +0100

    added passenger_ride network constraint

--- app/models/passenger_ride.rb
+++ app/models/passenger_ride.rb
@@ -2,4 +2,10 @@ class PassengerRide < ApplicationRecord
   belongs_to :user
   belongs_to :ride
   belongs_to :driver_ride, optional: true
+
+  validate :same_network, on: :create
+
+  def same_network
+    errors.add(:network, "must be on the same network") unless user.network_id == ride.network_id
+  end
 end
