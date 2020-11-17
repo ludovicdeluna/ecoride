62f4e1239b6204016465a87a2f877e2ea1a5e529
Ludovic de Luna <ludovic.deluna@gmail.com>
2020-11-17 01:11:12 +0100

    add network header

--- spec/requests/passenger_and_driver_share_a_ride_spec.rb
+++ spec/requests/passenger_and_driver_share_a_ride_spec.rb
@@ -6,9 +6,11 @@ RSpec.describe "Ride sharing between a driver and a passenger", :type => :reques
   let(:driver){ User.create!(network: network, email: "david@email.com") }
   let(:passenger){User.create!(network: network, email: "peter@email.com")}
   let(:ride){Ride.create!(network: network, departure: "ici", arrival: "la") }
+  let(:json_response) { JSON.parse(response.body) }
+  let(:network_header) { {"X-CURRENT-NETWORK" => network.id} }
 
   it "creates a Widget and redirects to the Widget's page" do
-    post "/graphql", params: {
+    post "/graphql", headers: network_header, params: {
       query: "mutation{
         createDriverRide(
           input: {
@@ -24,10 +26,17 @@ RSpec.describe "Ride sharing between a driver and a passenger", :type => :reques
       }"
     }
 
-    expect(DriverRide.last.user).to eq(driver)
-    expect(DriverRide.last.ride).to eq(ride)
+    expect(response.status).to eq(200)
+    expect(json_response.dig("data", "createDriverRide", "errors")).to eq([])
 
-    post "/graphql", params: {
+    driver_ride_id = json_response.dig("data", "createDriverRide", "driverRide", "id")
+    expect(driver_ride_id).not_to be(nil)
+
+    driver_ride = DriverRide.find(driver_ride_id)
+    expect(driver_ride.user).to eq(driver)
+    expect(driver_ride.ride).to eq(ride)
+
+    post "/graphql", headers: network_header, params: {
       query: "mutation{
         createPassengerRide(
           input: {
@@ -46,7 +55,7 @@ RSpec.describe "Ride sharing between a driver and a passenger", :type => :reques
     expect(PassengerRide.last.user).to eq(passenger)
     expect(PassengerRide.last.ride).to eq(ride)
 
-    post "/graphql", params: {
+    post "/graphql", headers: network_header, params: {
       query: "mutation{
         shareRide(
           input: {
